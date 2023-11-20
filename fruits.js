f86a4bc987fddb392e837a129761fa7980a9c0a3
譚丞岡  ChengKang Tan <87925027+TCK2001@users.noreply.github.com>
2023-11-20 20:00:02 +0800

    Update fruits.js

--- fruits.js
+++ fruits.js
@@ -1,51 +1,4 @@
-const FRUITS_BASE = [
-    {
-      name: "base/00_cherry",
-      radius: 33 / 2,
-    },
-    {
-      name: "base/01_strawberry",
-      radius: 48 / 2,
-    },
-    {
-      name: "base/02_grape",
-      radius: 61 / 2,
-    },
-    {
-      name: "base/03_gyool",
-      radius: 69 / 2,
-    },
-    {
-      name: "base/04_orange",
-      radius: 89 / 2,
-    },
-    {
-      name: "base/05_apple",
-      radius: 114 / 2,
-    },
-    {
-      name: "base/06_pear",
-      radius: 129 / 2,
-    },
-    {
-      name: "base/07_peach",
-      radius: 156 / 2,
-    },
-    {
-      name: "base/08_pineapple",
-      radius: 177 / 2,
-    },
-    {
-      name: "base/09_melon",
-      radius: 220 / 2,
-    },
-    {
-      name: "base/10_watermelon",
-      radius: 259 / 2,
-    },
-  ];
-  
-  const FRUITS_HLW = [
+ const FRUITS_HLW = [
     {
       name: "halloween/00_cherry",
       radius: 46 / 2,
@@ -72,7 +25,7 @@ const FRUITS_BASE = [
     },
     {
       name: "halloween/06_pear",
-      radius: 142 / 2,
+      radius: 200 / 2,
     },
     {
       name: "halloween/07_peach",
@@ -92,4 +45,4 @@ const FRUITS_BASE = [
     },
   ];
   
-  export { FRUITS_BASE, FRUITS_HLW };
+  export { FRUITS_HLW };
